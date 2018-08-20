4bb820afbe55c671cf9b9865e058b305d45e1081
GeorgePC <hamgeorge1990@gmail.com>
2018-08-21 00:18:37 +0800

    Add The Ribbon Demo

---
+++ WpfApplication1/Bussiness.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+namespace WpfApplication1
+{
+   public class Bussiness
+    {
+        public Bussiness()
+        {
+            MessageBox.Show("Hello Ribbon");
+        }
+    }
+}
